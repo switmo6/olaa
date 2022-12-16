9ece76b34c85edddce72b2bffcf17c5bc3501bf1
switmo6 <120728827+switmo6@users.noreply.github.com>
2022-12-16 15:07:26 +0100

    Add files via upload

---
+++ paragraph.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>paragraph example</title>
+	</head>
+	<body>
+		<p>This is my first paragraph</p>
+		<p>This is my second paragraph</p>
+		<p>This is my third paragraph</p>
+		
+		
+	</body>
+</html>
